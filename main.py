9e79a2c435f69a3d58f68028e930f64942a0cf91
filhaDeHades <43479786+filhaDeHades@users.noreply.github.com>
2018-10-31 15:54:48 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import estado
 from PPlay.window import *
+from PPlay.keyboard import *
 estadoJogo = 1
 
 roxo = [190, 117, 216]
@@ -10,7 +11,6 @@ janela = Window(500, 700)
 janela.set_background_color(roxo)
 
 while(1):
-
     if estadoJogo == 1:
         estadoJogo = estado.Menu(janela)
     elif estadoJogo == 2:
